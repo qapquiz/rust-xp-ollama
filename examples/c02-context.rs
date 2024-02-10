04c3e12fae60a56466b5dc29369d9a91105c067d
qapquiz <arm.armariya@gmail.com>
2024-02-10 23:05:37 +0700

    feat: add c02-context

---
+++ examples/c02-context.rs
@@ -0,0 +1,48 @@
+use futures::StreamExt;
+use tokio::io::AsyncWriteExt;
+use xp_ollama::consts::MODEL;
+use xp_ollama::gen::generation_stream_print;
+
+use ollama_rs::{
+    generation::completion::{request::GenerationRequest, GenerationContext, GenerationFinalResponseData, GenerationResponse},
+    Ollama,
+};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // By default localhost:11434
+    let ollama = Ollama::default();
+    let model = MODEL.to_string();
+    let prompts = &[
+        "Why the sky is red?",
+        "What was my first question?",
+    ];
+
+    let mut last_contexts: Option<Vec<GenerationContext>> = None;
+
+    for prompt in prompts {
+        println!("prompt >> {}", prompt);
+        let mut generation_request = GenerationRequest::new(model.to_owned(), prompt.to_string());
+
+        if let Some(last_contexts) = last_contexts.take() {
+            for last_context in last_contexts {
+                generation_request = generation_request.context(last_context);
+            }
+        }
+
+        let final_data = generation_stream_print(&ollama, generation_request).await;
+
+        if let Ok(final_data) = final_data {
+            let contexts: Vec<GenerationContext> = final_data.into_iter().map(|data| data.context).collect();
+            last_contexts = Some(contexts);
+        }
+    }
+
+    // Single Response Generation
+    // generation_stream_print(&ollama, generation_request).await?;
+    // let response = ollama.generate(generation_request).await?;
+    // println!("response: {}", response.response);
+
+    Ok(())
+}
